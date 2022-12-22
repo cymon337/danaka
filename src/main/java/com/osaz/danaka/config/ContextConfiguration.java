aa8398f612a5f197a7ff4ad6f0ce589cb4115770
cymon337 <113357179+cymon337@users.noreply.github.com>
2022-12-22 20:16:29 +0900

    feature : - application.yml DB 연결 설정 - pom.xml dependencies 추가 - ContextConfiguration.java 생성 - MainController.java 작성중
    ################
    메인컨트롤러 작성중

---
+++ src/main/java/com/osaz/danaka/config/ContextConfiguration.java
@@ -0,0 +1,9 @@
+package com.osaz.danaka.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.osaz.danaka")
+public class ContextConfiguration {
+}
