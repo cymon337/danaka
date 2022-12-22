8d9caea2bca4794ad04b5a9844f65da11ddff8fa
cymon337 <113357179+cymon337@users.noreply.github.com>
2022-12-22 19:53:39 +0900

    # feature : # Create project
    ################
    # 본문(추가 설명)을 아랫줄에 작성
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (관련된 이슈 번호 등 추가)
    
    ################

---
+++ src/test/java/com/osaz/danaka/DanakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osaz.danaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanakaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
