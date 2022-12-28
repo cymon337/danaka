dcf801d1dfa3735bb73004f5f647fea8a3f674cd
root <jgh@gmail.com>
2022-12-28 17:58:51 +0900

    2022-12-28 17:56
    # feature : 기능 구현중
    
    # docs : list2.css 지움
             porm.xml 시큐리티 활성화
    
    # style : 마이페이지 관심상품 디테일 수정

--- src/main/java/com/osaz/danaka/member/model/service/MemberService.java
+++ src/main/java/com/osaz/danaka/member/model/service/MemberService.java
@@ -1,4 +1,11 @@
 package com.osaz.danaka.member.model.service;
 
-public class MemberService {
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.List;
+import java.util.Map;
+
+public interface MemberService extends UserDetailsService {
+
+    Map<String, List<String>> getPermitListMap();
 }
