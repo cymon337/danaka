34c0d16947498b2dcb38246d74e6418fb98096b8
cymon337 <113357179+cymon337@users.noreply.github.com>
2023-01-06 15:11:31 +0900

    # style : # 조합견적 관련 DTO 생성 rod, reel, line

--- src/main/java/com/osaz/danaka/productPackage/controller/PackageController.java
+++ src/main/java/com/osaz/danaka/productPackage/controller/PackageController.java
@@ -16,13 +16,20 @@ public class PackageController {
     @GetMapping
     public String packageMain(){ return "package/package"; }
 
-//    1 상품명 검색
-//    2 옵션 필터
+//    1 상품명 검색 (카테고리 조건에 따라 로드, 릴, 라인 으로 검색)
+
+//    2 옵션 필터 (카테고리 조건에 따라 로드, 릴, 라인 옵션 호출)
+
 //    3 상품 목록
+
 //    4 견적 카트
+
 //    5 선택상품삭제
+
 //    6 전체삭제
+
 //    7 장바구니담기
+
 //    8 바로결제
 
 
